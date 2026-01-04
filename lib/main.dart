3440fa3e3d4ffe6aefe8126ff1c10136aec6ea49
hotdogpit <flame.grilled.hotdog@gmail.com>
2026-01-04 10:13:18 +0900

    Initial commit (exclude unity directory)

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:routine_app/screen/app.dart';
+import 'screen/city_unity_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: CityUnityPage(),
+    );
+  }
+}
