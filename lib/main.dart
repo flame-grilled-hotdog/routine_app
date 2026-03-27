b8831c59b366e326204ea75eddcbb67bf1ea1af4
go0105rev <go@revale.co.jp>
2026-03-27 16:51:19 +0900

    webブラウザ検証用設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:routine_app/screen/app.dart';
+import 'package:flutter/foundation.dart';
 import 'dart:io';
 
 void main() {
-  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+  if(kIsWeb){
+    ;
+  }else if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
     sqfliteFfiInit();
     databaseFactory = databaseFactoryFfi;
   }
